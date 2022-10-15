ba76d05ae623e89a3d04dc19a5ab7fc31fba6077
Antonio <antoniopsilva083@gmail.com>
2022-10-15 03:12:00 -0300

    feat: create font config

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
 import theme from './src/theme';
 
 import { Home } from './src/screens/Home';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      {fontsLoaded ? <Home /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
