84008a724d839a6b3dd350a9c0dfd86d197f89c7
Antonio <antoniopsilva083@gmail.com>
2022-10-16 01:57:10 -0300

    feat: create loading component

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
+import { Loading } from './src/components/Loading';
+
 import theme from './src/theme';
 
 import { Home } from './src/screens/Home';
-import { ActivityIndicator } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Home /> : <ActivityIndicator />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
 }
