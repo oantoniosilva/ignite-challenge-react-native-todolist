629951a6ee5f87e02395f4610e9f0291794660c7
Antonio <antoniopsilva083@gmail.com>
2022-10-17 00:32:43 -0300

    feat: style statusbar

--- App.tsx
+++ App.tsx
@@ -6,12 +6,18 @@ import { Loading } from './src/components/Loading';
 import theme from './src/theme';
 
 import { Home } from './src/screens/Home';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent 
+      />
       {fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
